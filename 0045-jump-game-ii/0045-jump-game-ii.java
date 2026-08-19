f68773a10d624bc9363b3523c095d68250ea2a09
santanu sahu <sahusantanu786@gmail.com>
2026-08-19 23:53:12 +0530

    Time: 1 ms (99.76%), Space: 47.2 MB (58.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
+        if(nums.length==1)
+        return 0;
          int index=0,greatestIndex=0,checkedIndex=0,step=0;
    while(index<nums.length){
     if(index+nums[index]>=nums.length-1)
@@ -8,9 +10,7 @@ class Solution {
     for(int j=index+nums[index]-1;j>checkedIndex;j--){
         if((j+nums[j])>greatestIndex+nums[greatestIndex])
             greatestIndex=j;
-       // System.out.println(j);
          }
-         System.out.println(greatestIndex);
          checkedIndex=index+nums[index];
          index=greatestIndex;
          step++;
