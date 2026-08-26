a4d2b50f5a02823bac34b1953a8be9c86e4588f6
santanu sahu <sahusantanu786@gmail.com>
2026-08-26 23:55:03 +0530

    Time: 7 ms (72.8%), Space: 49.4 MB (92.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,30 +1,12 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         List<List<String>> outerList=new ArrayList<>();
+      List<List<String>> outerList=new ArrayList<>();
         HashMap<String,List<String>> map=new HashMap<>();
-        int counter=0;
-       
-        StringBuilder sb=new StringBuilder();
         for(int i=0;i<strs.length;i++){
-            int[] frequencyMap=new int[26];
-            int len=strs[i].length();
-            //System.out.println(strs[i].charAt(len-1));
-
-           for(int j=0;j<len;j++){
-            ++frequencyMap[strs[i].charAt(j)-'a'];
-        //System.out.println(frequencyMap[strs[i].charAt(j)-'a']);
-        }
-           
-
-           for(int j=0;j<26;j++)
-            sb.append(frequencyMap[j]).append('#');
-
-         String buff=sb.toString();
-        // System.out.println(buff);
-         sb.setLength(0);
-        
-
-             if(map.containsKey(buff)){
+            char[] charArray=strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String buff=new String(charArray);
+           if(map.containsKey(buff)){
              map.get(buff).add(strs[i]);
             }
             else{
@@ -32,9 +14,6 @@ class Solution {
             }
         }
 
-        for(String key:map.keySet()){
-            outerList.add(map.get(key));
-        }
-return outerList;   
+       return new ArrayList<>(map.values());
     }
 }
