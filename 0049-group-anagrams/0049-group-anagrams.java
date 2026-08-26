435ca19e828d56bf6719b1802283608e1d3bc61f
santanu sahu <sahusantanu786@gmail.com>
2026-08-26 21:22:40 +0530

    Time: 8 ms (34.48%), Space: 49.5 MB (72.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,43 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         int[] compareMap;
-      List<String> innerList;
       List<List<String>> outerList=new ArrayList<>();
-      int lenPerString=strs[0].length();
+        HashMap<String,List<String>> map=new HashMap<>();
         for(int i=0;i<strs.length;i++){
-            if(strs[i]==null)
-                continue;
-
-           compareMap =new int[26];
-           innerList=new ArrayList<>();
-         for(int j=0;j<strs[i].length();j++){
-            System.out.println(strs[i].charAt(j)-'a');
-            compareMap[strs[i].charAt(j)-'a']++;
-         }
-         innerList.add(strs[i]);
-         for(int m=i+1;m<strs.length;m++){
-            if(strs[m]==null||strs[m].length()!=strs[i].length())
-            continue;
-
-            int n;
-            int[]  storeMap=new int[26];
-            for(n=0;n<strs[m].length();n++){
-                int storeCharacter=strs[m].charAt(n)-'a';
-                if(compareMap[storeCharacter]==storeMap[storeCharacter])
-                   break;
-                else 
-                   storeMap[storeCharacter]++; 
+            char[] charArray=strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String buff=new String(charArray);
+            if(map.containsKey(buff)){
+             map.get(buff).add(strs[i]);
             }
-            if(n==strs[m].length()){
-                innerList.add(strs[m]);
-            strs[m]=null;
+            else{
+                map.put(buff,new ArrayList<>(List.of(strs[i])));
             }
-         }
-         outerList.add(new ArrayList<>(innerList));
-
-
-         
         }
-        return outerList;
+
+        for(String key:map.keySet()){
+            outerList.add(map.get(key));
+        }
+return outerList;   
     }
 }
